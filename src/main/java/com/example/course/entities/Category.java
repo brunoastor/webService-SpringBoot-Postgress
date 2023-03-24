32d116a6e1045e883095a01cb38c8826ad04fbdb
bruno <bruno.ams@hotmail.com>
2023-03-24 15:45:05 -0300

    Many To Many

--- src/main/java/com/example/course/entities/Category.java
+++ src/main/java/com/example/course/entities/Category.java
@@ -5,10 +5,13 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -22,8 +25,9 @@ public class Category implements Serializable {
 	private Long id;
 	private String name;
 	
-	@Transient
-	private Set<Category> categories = new HashSet<>();
+	@JsonIgnore
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	
 	public Category() {		
@@ -48,10 +52,10 @@ public class Category implements Serializable {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-	
-	public Set<Category> getCategories() {
-		return categories;
+	}	
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
